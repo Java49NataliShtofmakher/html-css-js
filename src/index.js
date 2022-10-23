f4c2aa5d5035bab0bf2c089a11aaca818e7cc9af
Natali Shtofmakher <natalilg2010@gmail.com>
2022-10-23 12:31:44 +0300

    remove class, class hidden

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden"
 
 function setDetails (anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
-
 }
+
 for (let i = 0; i < anchors.length; i++){
     anchors[i].addEventListener("click", function(){
+        showDetails();
         setDetails(anchors[i]);
     })
+}
+function showDetails(){
+    mainClass.classList.remove(HIDDEN);
+}
+function hideDetails(){
+    mainClass.classList.add(HIDDEN)
 }
