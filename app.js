d65f7f1756967da427742f9a2ac8ad9f2e1cc067
Natali Shtofmakher <natalilg2010@gmail.com>
2022-11-07 22:20:11 +0200

    hw #14 arrays manipulation

--- app.js
+++ app.js
@@ -1,99 +1,56 @@
-// *********CHECK ID NUMBER *******************************************************************
-function checkNum(array) {               // check all elements of array that are numbers 
-    // function checkNum(element, index, array) {
-    // if (typeof +element !== 'number') {
-    //     console.log("Wrong ID number");
-    //     return;
-    // }
+let strings = ["abc", "lmn", "cd", "xyz", "123"];
 
-    let isError = false;
+function ulSurround(array) {
+    const start = "<li>";
+    const finish = "</li>";
+    array.push("</ul>");
+    array.splice(0, 0, "<ul>");
+
+    for (let i = 1; i < array.length - 1; i++) {
+        array[i] = start + array[i] + finish;
+    }
+    return array;
+}
+
+let ulSurrounding = ulSurround(strings);
+// console.log(ulSurrounding);
+
+//**********************************************************
+let strings1 = ["abc", "lmn", "cd", "abc", "abc"];
+
+function count(array, str) {
+    let counter = 0;
     for (let i = 0; i < array.length; i++) {
-        if (typeof +array[i] !== 'number') {
-            isError = true;
-            break;
-        }
+        if (array[i] === str)
+            counter++;
     }
-    return isError;
+    return counter;
 }
+// console.log(count(strings1, "abc"));
 
-function checkIdNumber(arID) {      // make operations with digits according to the algorithm 
-    return arID.map(function (num, index) {
-        if (index % 2 == 0) {
-            return num;
-        }
-        else {
-            num = num * 2;
-            if (num > 9) {
-                let secondDigit = num % 10;
-                let firstDigit = (num - secondDigit) / 10;
-                num = secondDigit + firstDigit;
-            }
-            return num;
-        }
+//**********************************************************
 
-    });
+let arS = [1, 2, 3, 4, 5, 6, 7];
+let arD = [10, 20, 30, 40, 50, 60, 70];
+function arrayCopy(src, srcPos, dst, dstPos, length) {
+    let copyPart = src.slice(srcPos, srcPos + length);
+    dst.splice(dstPos, 0, ...copyPart);
+
+    return dst;
 }
+// console.log(arrayCopy(arS, 3, arD, 4, 3));
 
-function getSum(arr) {      //make sum of digits in array
-    return arr.reduce(function (res, cur) {
-        return res + +cur;
-    }, 0)
-}
+//**********************************************************
+let numbers = [1, 2, 3, 4, 5, 6, 7];
 
-function checkTeudatZehut(teudatStrNumber) {
-    let IDstr = teudatStrNumber;
-    let arID = Array.from(IDstr);       // making array from given string
-
-    if (arID.length != 9) {         // checking of correct amount of digits
-        console.log("Wrong ID number length");
-        return;
+function move(array, index, offset) {
+    const offsetRule = index + offset;
+    if (index >= 0 && index < array.length && offsetRule > 0 && offsetRule < array.length) {
+        let cutPart = array.splice(index, 1);
+        array.splice(index + offset, 0, ...cutPart);
+        return array;
     }
-    // arID.forEach(checkNum); // I cant use method forEach() for checking if there are numbers in arrays, in case there is not, I cant break or stop the function. Thats why I did it another way.
-
-    const isError = checkNum(arID);
-    if (isError) return;
-
-    let arCheckedIDnumber = checkIdNumber(arID)
-
-    let sumIDnum = getSum(arCheckedIDnumber)
-
-    if (sumIDnum % 10 == 0) {       // check correctnes of ID
-        return true;
-    }
-    else {
-        return false;
-    }
+    return "ERROR"
 
 }
-
-console.log("The correctness of your ID -> ", checkTeudatZehut("342418944"));
-console.log("The correctness of your ID -> ", checkTeudatZehut("342418945"));
-
-// *****************GENERATE ID************************************************
-// second part of homework that Yuri said to do yesterday, but isnt written in dropbox, but i alreadu did it
-
-function createID() {
-    let newID = [];
-    for (let i = 0; i < 8; i++) {
-        newID[i] = Math.round(Math.random() * 9);
-    }
-    return newID;
-}
-
-function generateID() {
-    const ids = createID();
-    const newID = checkIdNumber(ids);
-    const sum = getSum(newID)
-    const x = sum % 10;
-
-    let ninthNumber = 0;
-    if (x !== 0) {
-        ninthNumber = 10 - x
-    }
-
-    ids[8] = ninthNumber
-    return ids.join("")
-}
-
-
-console.log("New generated ID -> ", generateID());
+// console.log(move(numbers, 2, 3));
